e58163efd835bcca215616c2ad2b510431b9c2d1
AllanAmaro <allan.amaro09@gmail.com>
2020-07-01 22:47:51 -0300

    Implementação parcial da funcionalidade de combate por dados

--- js/event.js
+++ js/event.js
@@ -2,44 +2,40 @@ var jogador = {
   apelido: ''
 }
 
+const TEMPO_INTERVALO_EXIBICAO_TEXTO = 0.0035;
 var valorIteracaoExibicaoTexto = 0;
 var intervaloExibicaoTexto;
 var textoParaExibir;
+var textoExibido = false;
 var elementoHTML_Texto;
 var elementoHTML_ListaDeAcoes;
+var elementoHTML_ListaDeInimigos;
 var roteiroSelecionado;
 var idCenaSelecionada = 1;
 var listaDeAcoes = [];
+var telaRolagemDados;
+var acaoImediata;
+var cenaParaExibir;
+var inimigos;
+var listaDeInimigos = [];
 
 function carregar() {
   roteiroSelecionado = roteiro;
   elementoHTML_Texto = document.getElementById('texto');
   elementoHTML_ListaDeAcoes = document.getElementById('lista-de-acoes');
+  elementoHTML_ListaDeInimigos = document.getElementById('lista-de-inimigos');
+  telaRolagemDados = document.getElementById('tela-rolagem-dados');
   exibeHistoria();
 }
 
-function exibeHistoria() {
+async function exibeHistoria() {
+
   limparCenaAnterior();
+  selecionaCenaParaExibir();
   selecionaTextoParaExibir();
-  intervaloExibicaoTexto = setInterval(exibeTextoPorCaracter, 4);
-}
-
-function exibeTextoPorCaracter() {
-  elementoHTML_Texto.innerHTML += textoParaExibir.split('')[valorIteracaoExibicaoTexto];
-  valorIteracaoExibicaoTexto++;
-  if (valorIteracaoExibicaoTexto === textoParaExibir.length) {
-    paraExibicao();
-  }
-}
-
-function paraExibicao() {
-  clearInterval(intervaloExibicaoTexto);
-  exibeAcoes();
-}
-
-function proximaCena(proximaCena) {
-  idCenaSelecionada = proximaCena;
-  exibeHistoria();
+  await exibeTextoCena();
+  selecionaTipoAcao();
+  
 }
 
 function limparCenaAnterior() {
@@ -50,24 +46,58 @@ function limparCenaAnterior() {
   listaDeAcoes.length = 0;
 }
 
-function selecionaTextoParaExibir() {
+function selecionaCenaParaExibir() {
   for (let i = 0; i < roteiroSelecionado.cenas.length; i++) {
     if (roteiroSelecionado.cenas[i].cena.id === idCenaSelecionada) {
-      textoParaExibir = roteiroSelecionado.cenas[i].cena.texto;
-      listaAcoes(roteiroSelecionado.cenas[i].cena.acoes);
+      cenaParaExibir = roteiroSelecionado.cenas[i].cena;  
     }
   }
 }
 
-function listaAcoes(acoes) {
-  for (let i = 0; i < acoes.length; i++) {
+function selecionaTextoParaExibir() {
+  textoParaExibir = cenaParaExibir.texto;
+}
+
+async function exibeTextoCena() {
+  let tamanhoTexto = textoParaExibir.split('').length;
+  let texto = textoParaExibir.split('');
+  for (let i = 0; i < tamanhoTexto; i++) {
+    let caracter = texto[i];
+    await exibeCaracter(caracter);
+  }
+
+  return 0;
+}
+
+async function exibeCaracter(char) {  
+  return new Promise(resolve => {
+		setTimeout(() => {
+			resolve(elementoHTML_Texto.innerHTML += char);
+		}, TEMPO_INTERVALO_EXIBICAO_TEXTO);
+	});
+}
+
+function selecionaTipoAcao() {
+  acaoImediata = cenaParaExibir.acaoImediata;
+  if (acaoImediata === null) {
+    carregaAcoesCena();
+    exibeAcoes();
+  } else {
+    if (acaoImediata.tipoAcao === 'COMBATE') {
+      iniciaCombate();
+    }
+  }
+}
+
+function carregaAcoesCena() {
+  for (let i = 0; i < cenaParaExibir.acoes.length; i++) {
     var embalagemAcao = document.createElement("div");
     var acao = document.createElement("button");
     embalagemAcao.classList.add('embalagem-acao');
     acao.classList.add('acao');
-    acao.innerHTML = acoes[i].texto;
+    acao.innerHTML = cenaParaExibir.acoes[i].texto;
     acao.onclick = function() {
-      proximaCena(acoes[i].idProximaCena);
+      proximaCena(cenaParaExibir.acoes[i].idProximaCena);
     }
     embalagemAcao.appendChild(acao);
     listaDeAcoes.push(embalagemAcao);
@@ -79,3 +109,97 @@ function exibeAcoes() {
     elementoHTML_ListaDeAcoes.appendChild(listaDeAcoes[i]);
   }
 }
+
+function proximaCena(proximaCena) {
+  idCenaSelecionada = proximaCena;
+  exibeHistoria();
+}
+
+function iniciaCombate() {
+    carregaInimigos();
+    exibeInimigos();
+}
+
+function carregaInimigos() {
+  inimigos = acaoImediata.inimigos;
+  for (let i = 0; i < inimigos.length; i++) {
+    var divInimigo = document.createElement("div");
+    divInimigo.classList.add('inimigo');
+
+    var vida = document.createElement("label");
+    vida.innerHTML = inimigos[i].vida;
+    divInimigo.appendChild(vida);
+
+    var dano = document.createElement("label");
+    dano.innerHTML = inimigos[i].dano;
+    divInimigo.appendChild(dano);
+
+    var acao = document.createElement("button");
+    acao.innerHTML = 'Atacar';
+    acao.onclick = function() {
+      exibeTelaRolagemDado();
+    }
+    divInimigo.appendChild(acao);
+    listaDeInimigos.push(divInimigo);
+  }
+}
+
+function exibeInimigos() {
+  for (let i = 0; i < listaDeInimigos.length; i++) {
+    elementoHTML_ListaDeInimigos.appendChild(listaDeInimigos[i]);
+  }  
+}
+
+function exibeTelaRolagemDado() {
+  telaRolagemDados.style.display = 'flex';
+}
+
+function escondeTelaRolagemDado() {
+  telaRolagemDados.style.display = 'none';
+}
+
+
+
+
+
+
+
+
+
+
+
+
+///////////TODO REMOVER
+
+/*function exibeAcoes() {
+  for (let i = 0; i < listaDeAcoes.length; i++) {
+    elementoHTML_ListaDeAcoes.appendChild(listaDeAcoes[i]);
+  }
+}*/
+
+/*function exibeTextoCena() {
+  intervaloExibicaoTexto = setInterval(exibeTextoPorCaracter, TEMPO_INTERVALO_EXIBICAO_TEXTO);
+}*/
+
+/*
+function acaoImediata() {
+  if (cenaParaExibir.acaoImediata) {
+    return true;
+  } else {
+    return false;
+  }
+}*/
+
+/*function exibeTextoPorCaracter() {
+  elementoHTML_Texto.innerHTML += textoParaExibir.split('')[valorIteracaoExibicaoTexto];
+  valorIteracaoExibicaoTexto++;
+  if (valorIteracaoExibicaoTexto === textoParaExibir.length) {
+    paraExibicao();
+  }
+}*/
+
+/*function paraExibicao() {
+  clearInterval(intervaloExibicaoTexto);
+  textoExibido = true;
+  // exibeAcoes();
+}*/
--- js/roteiro.js
+++ js/roteiro.js
@@ -8,6 +8,25 @@ var roteiro = {
                         'Handsome acorda meio atordoado com a cabeça doendo sem se lembrar de nada. Ele olha ao seu redor e logo percebe que está em uma cela com um tremendo clarão de luz vindo de uma das paredes, ele então se levante ainda meio atordoado e canbaleando ele continua a ouvir uma voz, a do outro prisioneiro na outra cela. \n' +
                         '[Desconhecido]: VAMOS CARA NÃO PERCA TEMPO TIRA A GENTE DAQUI!!! Você quer terminar igual ao cara ai ao seu lado? \n' +
                         'Handsome olha para o lado e ve um corpo de uma homem sem a cabeça, ele então se assusta e sai andando de costa até trombar na parede onde está o buraco que vinha o clarão.',
+                //acaoImediata: null,
+                acaoImediata: {
+                    tipoAcao: 'COMBATE',
+                    inimigos: [
+                        {
+                            id: 1,
+                            vida: 50,
+                            dano: 15
+                        },
+                        {
+                            id: 1,
+                            vida: 50,
+                            dano: 15
+                        }
+                    ],
+                    tipoDoDado: 1, //1 Equivale a 1d6
+                    condicaoSucesso: null,
+                    idProximaCena: 2
+                },
                 acoes: [
                     {
                         id: 1,
@@ -30,6 +49,7 @@ var roteiro = {
                         'Handsome então olha para a grade da cela e ve que ela foi parcialmente arracanda e pergunta. \n' +
                         '[Handsome]: Oque está acontecendo? \n' +
                         '[Roger]: POHA OQUE É VOCÊ UM DESSES ESCRITOR DE JORNAIS, VAMOS LOGO TIRA A GENTE DAQUI!! \n',
+                acaoImediata: null,
                 acoes: [
                     {
                         id: 1,
@@ -40,11 +60,6 @@ var roteiro = {
                         id: 2,
                         texto: 'Sair da cela e ir abrir as outras celas.',
                         idProximaCena: 5
-                    },
-                    {
-                        id: 3,
-                        texto: 'Perguntar novamente oque está acontecendo.',
-                        idProximaCena: 0
                     }
                 ]
             }
@@ -56,6 +71,7 @@ var roteiro = {
                         '[Desconhecido]: Está gostando da vistá? \n' +
                         '[Handsome]: A cidade está sendo atacada! \n' +
                         '[Desconhecido]: Aaaa não brinca, foi difícil de descobrir isso?!! AGORA VAI! SUA CELA ESTÁ ABERTA!! no final do corredor tem uma alavanca para abrir as outras celas incluindo a minha, VAMOS LOGO!! \n',
+                acaoImediata: null,
                 acoes: [
                     {
                         id: 1,
@@ -65,7 +81,7 @@ var roteiro = {
                     {
                         id: 2,
                         texto: 'Sair da cela e ir abrir as outras celas.',
-                        idProximaCena: 0
+                        idProximaCena: 6
                     }
                 ]
             }
@@ -76,11 +92,12 @@ var roteiro = {
                 texto: 'Handsome sai de sua cela e corre na direção da saida ao final do corredor, mas decide não libertar os prisioneiros. \n' +
                         'Ao sair pela porta no final do corredor ele se depara com um salão o qual possui uma grande estrutura de escadas no centro e portas nas paredes laterais, com 5 metros de distância para as escadas, muito provavelmente essas portas eram outras celas. \n' + 
                         'Handsome corre em direção as escadas.',
+                acaoImediata: null,
                 acoes: [
                     {
                         id: 1,
                         texto: 'Subir as escadas.',    
-                        idProximaCena: 0
+                        idProximaCena: 7
                     },
                     {
                         id: 2,
@@ -93,7 +110,7 @@ var roteiro = {
         {
             cena: {
                 id: 5,
-                texto: 'Handsome sai de sua cela e corre na direção da saida ao final do corredor e abre as celas no entanto apenas Roger e o outro infeliz da cela do Handsome estvam presos ali. \n' +
+                texto: 'Handsome sai de sua cela e corre na direção da saida ao final do corredor e abre as celas no entanto apenas Roger e o outro infeliz da cela do Handsome estavam presos ali. \n' +
                         'Ao sair pela porta no final do corredor ele e Roger se deparam com um salão o qual possui uma grande estrutura de escadas no centro e portas nas paredes laterais, com 5 metros de distância para as escadas, muito provavelmente essas portas eram outras celas. \n' + 
                         'Handsome e Roger correm em direção as escadas, Handsome começa a descer, mas para imediatamente assim que Roger avisa ele. \n' +
                         '[Roger]: PARA! PARA! Por ai não, temos que subir é a melhor saida! \n' +
@@ -101,6 +118,7 @@ var roteiro = {
                         '[Roger]: Antes de ser preso, eu trabalhava na limpeza desse lugar. A cidade está sobre ataque os guardas todos devem estar nos anderes abaixo. \n' +
                         '[Handsome]: Mas como vamos sair lá por cima? \n' +
                         '[Roger]: Essa torre está encostada em uma montanha, 4 andares acima da gente existe uma sala que guarda materiais de limpeza, lá tem uma janela muito fácil de quebrar. Tudo que precisamos fazer é quebrar a janela e cameçarmos a escalar a montanha. \n',
+                acaoImediata: null,
                 acoes: [
                     {
                         id: 1,
@@ -113,6 +131,49 @@ var roteiro = {
                         idProximaCena: 0
                     }
                 ]
+            }
+        },
+        {
+            cena: {
+                id: 6,
+                texto: 'Handsome sai de sua cela e corre na direção da saida ao final do corredor e abre as celas no entanto apenas o desconhecido e o outro infeliz da cela do Handsome estavam presos ali. \n' +
+                        'O desconhecido corre em direção a saída e para para agradecer Handsome. \n' +
+                        '[Desconhecido]: Boa cara obrigado! aproposito meu nome é Roger. Agora vamos! precisamos sair daqui. \n' +
+                        '[Handsome]: Eu me chamado Handsome. \n' +
+                        '[Roger]: Legal agora vamos! \n' +
+                        'Ao sair pela porta no final do corredor ele e Roger se deparam com um salão o qual possui uma grande estrutura de escadas no centro e portas nas paredes laterais, com 5 metros de distância para as escadas, muito provavelmente essas portas eram outras celas. \n' + 
+                        'Handsome e Roger correm em direção as escadas, Handsome começa a descer, mas para imediatamente assim que Roger avisa ele. \n' +
+                        '[Roger]: PARA! PARA! Por ai não, temos que subir é a melhor saida! \n' +
+                        '[Handsome]: Como você sabe? \n' +
+                        '[Roger]: Antes de ser preso, eu trabalhava na limpeza desse lugar. A cidade está sobre ataque os guardas todos devem estar nos anderes abaixo. \n' +
+                        '[Handsome]: Mas como vamos sair lá por cima? \n' +
+                        '[Roger]: Essa torre está encostada em uma montanha, 4 andares acima da gente existe uma sala que guarda materiais de limpeza, lá tem uma janela muito fácil de quebrar. Tudo que precisamos fazer é quebrar a janela e cameçarmos a escalar a montanha. \n',
+                acaoImediata: null,
+                acoes: [
+                    {
+                        id: 1,
+                        texto: 'Seguir Roger.',    
+                        idProximaCena: 0
+                    },
+                    {
+                        id: 2,
+                        texto: 'Descer as escadas.',
+                        idProximaCena: 0
+                    }
+                ]
+            }
+        },
+        {
+            cena: {
+                id: 7,
+                texto: 'Handsome começa a subir as escadas de forma rápida e ligeira, ao terminar de subir os 3 andares ele se depara com um guarda. \n',
+                acaoImediata: {
+                    tipoAcao: 'COMBATE',
+                    tipoDoDado: 1, //1 Equivale a 1d6
+                    condicaoSucesso: '>3',
+                    idProximaCena: 0
+                },
+                acoes: null
             }
         }
     ]
